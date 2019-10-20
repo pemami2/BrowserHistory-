8f55e5fa903e1f69e114fd99767e8c3f38427748
pemami2 <51972919+pemami2@users.noreply.github.com>
2019-10-20 14:25:00 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -1,11 +1,18 @@
+
 #pragma once
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iterator>     // std::next, std::prev
+#include <iterator>
+#include <list>
+#include "Site.h"
+
 
 using std::string;
+using std::list;
+using namespace std;
+
 
 class BrowserHistory {
 public:
@@ -22,5 +29,91 @@ public:
     size_t getHistoryLength();
 
 private:
-  // TO BE COMPLETED
+    std::list<Site> mylist;
+    list<Site>:: iterator nullnode = mylist.begin();
+    list<Site>:: iterator iter1 = mylist.begin();
+    list<Site>:: iterator iter2 = mylist.end();
+
 };
+
+BrowserHistory::BrowserHistory()
+    {
+    Site *i = new Site;
+    i->setUrl("");
+    i->setFilesize (0);
+    mylist.push_front(*i);
+    iter1++;
+    nullnode++;
+
+    }
+
+void BrowserHistory::visitSite(string url, size_t filesize){
+    if (iter1 !=iter2){
+        mylist.erase(std::next(iter1),iter2);
+    }
+
+    Site *w= new Site;
+    w->setUrl(url);
+    w->setFilesize (filesize);
+    mylist.push_back(*w);
+    iter1++;
+}
+
+void BrowserHistory::backButton(){
+
+    if (iter1 != std::next(nullnode)){
+        iter1 --;
+    }
+
+
+    }
+
+
+
+void BrowserHistory::forwardButton(){
+    if (iter1!= std::prev(iter2)){
+        iter1++;
+    }
+}
+
+void BrowserHistory::readFile(string fileName)
+{
+    string var = "";
+    ifstream inFile;
+    string url_from_file = "";
+    size_t filesize_from_file;
+    inFile.open(fileName);
+    while (!inFile.eof()){
+            inFile>>var;
+            if (var == "visit")
+            {
+                inFile>> url_from_file;
+                inFile>>filesize_from_file;
+                visitSite(url_from_file,filesize_from_file);
+            }
+            else if(var == "back")
+            {
+                backButton();
+            }
+            else if (var =="forward")
+            {
+                forwardButton();
+
+            }
+    }
+}
+
+string BrowserHistory::currentUrl(){
+    string urlReturn = iter1->getUrl();
+    return urlReturn;
+}
+
+size_t BrowserHistory::currentPageSize(){
+    size_t pageSizeReturn = iter1->getFilesize();
+    return pageSizeReturn;
+}
+
+size_t BrowserHistory::getHistoryLength(){
+    int sz = mylist.size() - 1;
+    return sz;
+}
