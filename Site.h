24e37bc05483dc3d755700f3fef09c58c42a5419
pemami2 <51972919+pemami2@users.noreply.github.com>
2019-10-20 14:27:11 -0700

    Create Site.h

---
+++ Site.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <list>
+
+using std::string;
+using std::list;
+
+class Site {
+private:
+    string url_ = "";
+    size_t filesize_ = 0;
+
+
+public:
+    Site() = default;
+    Site(string url, size_t filesize) : url_(url), filesize_(filesize){};
+
+    void setUrl ( string url )  {url_ = url; }
+    void setFilesize( size_t filesize ) {filesize_=filesize; }
+
+    string getUrl() const {return url_;}
+    size_t getFilesize () const {return filesize_;}
+
+};
